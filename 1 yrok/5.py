15cc21ed701ec28dd1cef4910972b6eac0faf2d0
setborg129 <setborg@inbox.ru>
2022-05-23 11:43:47 +0500

    Исправление 5

--- 1 yrok/5.py
+++ 1 yrok/5.py
@@ -1,18 +1,34 @@
 # yandex.ru, youtube.com
 
 import os
+import platform
+import subprocess
+from chardet import detect
 
-ip_list = input("Введите IP адрес: ")
-
-response = os.popen(f'ping {ip_list}').read()
-
-# response = os.popen(f'ping ya.ru').read()
-
-print(response)
-
-result_encode = response.encode(encoding='cp1251')
-print(result_encode)
-result_decode = result_encode.decode(encoding='cp866')
+urls = ['ya.ru', 'vk.com']
+code = '-n' if platform.system().lower() == 'windows' else '-c'
 
 
-print(result_decode)
+for item_url in urls:
+    ur = ['ping',code, '4', item_url]
+    ya_ping = subprocess.Popen(ur, stdout=subprocess.PIPE)
+    for line in ya_ping.stdout:
+        result = detect(line)
+        print(result)
+        line = line.decode(result['encoding']).encode('utf-8')
+        print(line.decode('utf-8'))
+
+
+
+# response = os.popen(f'ping {ip_list}').read()
+#
+# # response = os.popen(f'ping ya.ru').read()
+#
+# print(response)
+#
+# result_encode = response.encode(encoding='cp1251')
+# print(result_encode)
+# result_decode = result_encode.decode(encoding='cp866')
+#
+#
+# print(result_decode)
