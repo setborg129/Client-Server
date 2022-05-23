c97cc4cb5f0a72dd7b7d8e5c310f0998d790a448
setborg129 <setborg@inbox.ru>
2022-05-23 11:18:01 +0500

    Исправление

--- 1 yrok/2.py
+++ 1 yrok/2.py
@@ -2,14 +2,35 @@
 #2
 
 
-first_word = b'class'
-print(type(first_word))
-print(len(first_word))
+# first_word = b'class'
+# print(type(first_word))
+# print(len(first_word))
+#
+# second_word = b'function'
+# print(type(second_word))
+# print(len(second_word))
+#
+# third_word = b'method'
+# print(type(third_word))
+# print(len(third_word))
 
-second_word = b'function'
-print(type(second_word))
-print(len(second_word))
 
-third_word = b'method'
-print(type(third_word))
-print(len(third_word))
+A = 'class'
+B = 'function'
+C = 'method'
+str_list = [A,B,C]
+
+for item in str_list:
+    el = eval(f'b"{item}"')
+    el_tmp = (f"b'{item}'")
+    print(el)
+    print(el_tmp)
+ # в данном примере я так и не понял зачем используется тут метод "eval", разницы я так и не увидел.
+
+    # print('=' * 50)
+    # print('type: ', type(el))
+    # print(item)
+    # print('in bytes:', len(el))
+
+
+# Спасибо за пример. Теперь урок освоил.
