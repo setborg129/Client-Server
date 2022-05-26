64cecdade0a9c6d65e3863b356f34c002e339d81
setborg129 <setborg@inbox.ru>
2022-05-26 16:16:34 +0500

    До работка 1 части

--- 2 Yrok/1.py
+++ 2 Yrok/1.py
@@ -11,17 +11,66 @@
 # через вызов функции get_data(), а также сохранение подготовленных данных в соответствующий CSV-файл;
 # Проверить работу программы через вызов функции write_to_csv().
 
-file_text = ['info_1.txt', 'info_2.txt', 'info_3.txt']
+import re
+import pandas as df
 
+file_text = ['info_1.txt', 'info_2.txt', 'info_3.txt']
+list_all = []
+os_prod_list = []
+os_name_list = []
+os_code_list = []
+os_type_list = []
 
 def get_data(text_file):
     for text in text_file:
         with open(text, 'r', encoding='cp1251') as f:
             lines = f.readlines()
             for line in lines:
-                print(line.strip())
+                find_word = re.search(r'Название ОС:', line)
+                if find_word != None:
+                    os_name_list.append(line[34:].replace('\n', ''))
+                    list_all.append('\nНазвание ОС: ')
+                    list_all.append(line[34:].replace('\n', ''))
 
+                find_word = re.search(r'Изготовитель системы:', line)
+                if find_word != None:
+                    os_prod_list.append(line[34:].replace('\n',''))
+                    list_all.append('\nИзготовитель системы: ')
+                    list_all.append(line[34:].replace('\n',''))
+
+                find_word = re.search(r'Код продукта:', line)
+                if find_word != None:
+                    os_code_list.append(line[34:].replace('\n', ''))
+                    list_all.append('\nКод продукта: ')
+                    list_all.append(line[34:].replace('\n', ''))
+
+                find_word = re.search(r'Тип системы:', line)
+                if find_word != None:
+                    os_type_list.append(line[34:].replace('\n', ''))
+                    list_all.append('\nТип системы: ')
+                    list_all.append(line[34:].replace('\n', ''))
+
+
+    with open('main_data.txt','w', encoding='windows-1251' ) as file:
+        for item in list_all:
+            writen = file.write(item)
     f.close()
+    print(list_all)
+    # переводим list в excel файл !
+    # write_to_csv(list_all)
+
+def write_to_csv(list_csv):
+    pass
+    # good_list_csv = []
+    # for number, line in enumerate(list_csv):
+    #     good_list_csv.append({
+    #         'Название ОС:': os_name_list,
+    #         'Код продукта:': os_prod_list,
+    #         'Изготовитель системы:': os_code_list,
+    #         'Тип системы:': os_type_list,
+    #     })
+    # df.to_excel('write_to_xlsx.xlsx', index=False)
+    # print(f'Файл write_to_xlsx.xlsx успешно записан !')
 
 
 get_data(file_text)
