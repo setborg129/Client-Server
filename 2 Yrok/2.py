e815a32e37b40a0ad7a2c0b57e2baaba7c72924a
setborg129 <setborg@inbox.ru>
2022-05-30 14:50:44 +0500

    2-е задание

---
+++ 2 Yrok/2.py
@@ -0,0 +1,20 @@
+# 2. Задание на закрепление знаний по модулю json. Есть файл orders в формате JSON с информацией о заказах.
+# Написать скрипт, автоматизирующий его заполнение данными. Для этого:
+
+import json
+def write_order_to_json(item, quantity, price, buyer, date):
+    with open('orders.json', 'r', encoding='cp1251') as f_out:
+        data = json.load(f_out)
+
+    with open('orders.json', 'w', encoding='windows-1251') as file:
+        dict_list = {'item': item,
+                     'quantity': quantity,
+                     'price': price,
+                     'buyer': buyer,
+                     'date': date}
+        order_list = data["orders"]
+        order_list.append(dict_list)
+
+#
+write_order_to_json('msg', 'sng', '100', 'Petrov.A.A', '26.05.2022')
+
