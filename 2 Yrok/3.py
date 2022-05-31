94b0d6390d0df351f93d16d6d9f330d396fe7c4d
setborg129 <setborg@inbox.ru>
2022-05-31 14:35:30 +0500

    3- исправление

--- 2 Yrok/3.py
+++ 2 Yrok/3.py
@@ -2,14 +2,14 @@ import yaml
 
 data_into = {'keys':['Apply', 'Orange'],
              'item_count': 2,
-             'items_price': {'Apply': '150',
-                             'Orange': '53',
+             'items_price': {'Apply': '150$',
+                             'Orange': '53€',
                             }
              }
 
 
 with open('test.yaml', 'w', encoding='utf-8') as f:
-    yaml.dump(data_into, f)
+    yaml.dump(data_into, f, default_flow_style = False, allow_unicode = True)
 
 
 with open('test.yaml', 'r', encoding='utf-8') as f_o:
